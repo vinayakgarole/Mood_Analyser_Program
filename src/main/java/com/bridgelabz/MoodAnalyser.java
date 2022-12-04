8ea8d48f18aeccc5297a70a99df2b0b36d17284d
vinayakgarole <vinayakgarole10@gamil.com>
2022-12-04 14:34:00 +0530

    Added: Happy or Sad Mood

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,4 +1,13 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+
+    public String analyseMood(String message) {
+
+        if (message.contains("sad")) {
+            return "SAD";
+        } else {
+            return "HAPPY";
+        }
+    }
 }
--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,4 +1,23 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest {
+
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a sad message");
+        System.out.println(mood);
+        Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is a happy message");
+        System.out.println(mood);
+        Assertions.assertEquals("HAPPY", mood);
+    }
 }
